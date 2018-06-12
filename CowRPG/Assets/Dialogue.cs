9a320caa1cd336ca22bde24b263b628f1899cb1e
Cowlord3001 <christophera1225@aol.com>
2018-06-12 15:24:23 -0400

    Dialogue WIP

---
+++ CowRPG/Assets/Dialogue.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour {
+
+    public TextAsset TextFile;
+    public string[] TextLines;
+    public Text CurrentText;
+    int Line;
+    int Character;
+    public float TextSpeed;
+
+	// Use this for initialization
+	void OnEnable() {
+		if(TextFile != null)
+        {
+            Debug.Log("awake");
+            CurrentText.text = "";
+            Line = 0;
+            Character = 0;
+            TextLines = TextFile.text.Split('\n');
+            InvokeRepeating("UpdateText", 0, 1/TextSpeed);
+        }
+	}
+
+    void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+
+    void UpdateText()
+    {
+        if(Character > TextLines[Line].Length-1)
+        {
+            Line = Line + 1;
+            Character = 0;
+            CurrentText.text = CurrentText.text + '\n';
+        }
+        if (Line > TextLines.Length-1)
+        {
+            CancelInvoke();
+        }
+        else if(Line == TextLines.Length-1 && TextLines[Line].Length - 1 <= 0)
+        {
+            CancelInvoke();
+        }
+        else
+        {
+            CurrentText.text = CurrentText.text + TextLines[Line][Character];
+            Character = Character + 1;
+        }
+    }
+}
---
+++ CowRPG/Assets/TextOrder.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextOrder : MonoBehaviour {
+
+    public Transform [] Pages;
+    int CurrentPage;
+    int LastPage;
+
+	// Use this for initialization
+	void Start () {
+        Pages = GetComponentsInChildren<Transform>();
+        LastPage = Pages.Length - 1;
+        CurrentPage = 3;
+        Debug.Log(LastPage);
+        for (int i = 4; i <= LastPage; i++)
+        {
+            Pages[i].gameObject.SetActive(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            OpenNextPage();
+        }
+	}
+
+    void OpenNextPage()
+    {
+        if (CurrentPage == LastPage)
+        {
+            CloseDialogue();
+        }
+        else
+        {
+            CurrentPage = CurrentPage + 1;
+            Pages[CurrentPage].gameObject.SetActive(true);
+            Pages[CurrentPage - 1].gameObject.SetActive(false);
+        }
+    }
+
+    public void CloseDialogue()
+    {
+        gameObject.SetActive(false);
+    }
+}
