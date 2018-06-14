c80fd00d238efdc99c8d1fad0855212cd3ec2880
Cowlord3001 <christophera1225@aol.com>
2018-06-14 15:24:13 -0400

    Classes & that dang '

--- CowRPG/Assets/Scripts/Arena/Firing.cs
+++ CowRPG/Assets/Scripts/Arena/Firing.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Firing : MonoBehaviour {
 
-    public GameObject Bullet;
+    public GameObject RangedBullet;
+    public GameObject MeleeBullet;
+    public GameObject SpellBullet;
     public float ROF;
     float Timestamp;
+    bool Charging;
+    GameObject GO;
+    Vector3 Scale;
 
 	// Use this for initialization
 	void Start () {
@@ -15,11 +20,60 @@ public class Firing : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.Mouse0) && Timestamp + ROF < Time.time)
+        if(gameObject.tag == "Ranger")
         {
-            Instantiate(Bullet, transform.position, transform.parent.transform.rotation);
+            RangerFire();
+        }
+        else if(gameObject.tag == "Fighter")
+        {
+            FighterFire();
+        }
+        else if(gameObject.tag == "Spellcaster")
+        {
+            SpellFire();
+        }
+	}
+
+    void RangerFire()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && Timestamp + ROF < Time.time)
+        {
+            Instantiate(RangedBullet, transform.position, transform.parent.transform.rotation);
             Timestamp = Time.time;
         }
+    }
 
-	}
+    void FighterFire()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && Timestamp + ROF*4 < Time.time)
+        {
+            Instantiate(MeleeBullet, transform.position, transform.parent.transform.rotation);
+            Timestamp = Time.time;
+        }
+    }
+
+    void SpellFire()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Timestamp + ROF * 8 < Time.time)
+        {
+            GO = Instantiate(SpellBullet, transform.position, transform.parent.transform.rotation);
+            Timestamp = Time.time;
+            Charging = true;
+            Scale = GO.transform.localScale;
+        }
+        else if(Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            GO.GetComponent<Rigidbody2D>().velocity = transform.right * 5;
+            GO.transform.parent = null;
+            Charging = false;
+        }
+        if(Charging == true)
+        {
+            GO.transform.position = transform.position + transform.right;
+            GO.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+            Debug.Log(GO.transform.localScale);
+            GO.transform.localScale = GO.transform.localScale + Scale * Time.deltaTime * .5f;
+        }
+    }
 }
