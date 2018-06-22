2fd7c3182048a593d1f5820c8e28da00d5e178ba
Garnett42 <34786138+Garnett42@users.noreply.github.com>
2018-06-22 12:41:44 -0400

    Random Maze FrameWork

---
+++ CowRPG/Assets/GenerateMaze.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum Facing
+{
+    Up, Right, Down, Left
+}
+
+
+public class GenerateMaze : MonoBehaviour {
+
+    public int Width, Height;
+    public float tileWidth, tileHeight;
+
+    GameObject[,] Maze;
+
+    public GameObject TileGameObject;
+
+	// Use this for initialization
+	void Start () {
+        Maze = new GameObject[Width, Height];
+
+        for (int x = 0; x < Width; x++)
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                Maze[x,y] = Instantiate(TileGameObject,
+                                        (Vector2)transform.position + new Vector2(x * tileWidth, y * tileHeight),
+                                        Quaternion.identity);
+
+                
+                Maze[x,y].transform.parent = transform;
+                Maze[x, y].GetComponent<Tile>().MazeGen = gameObject;
+                Maze[x,y].name = "Tile_" + x + "" + y;
+
+                Debug.Log("Tile_" + x + "" + y + " Instantiated");
+
+            }
+        }
+
+        LinkNeighbors();
+	}
+	
+
+    void LinkNeighbors()
+    {
+        for (int x = 0; x < Width; x++)
+        {
+            for (int y = 0; y < Height; y++)
+            {
+
+                if (x == 0 && y == 0)
+                {
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Up] = Maze[x, y + 1].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Right] = Maze[x + 1, y].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Down] = null;
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Left] = null;
+                }
+                else if (x == 0 && y == Height - 1)
+                {
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Up] = null;
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Right] = Maze[x + 1, y].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Down] = Maze[x, y - 1].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Left] = null;
+                }
+                else if (x == Width - 1 && y == 0)
+                {
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Up] = Maze[x, y + 1].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Right] = null;
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Down] = null;
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Left] = Maze[x - 1, y].GetComponent<Tile>();
+                }
+                else if (x == Width - 1 && y == Height - 1)
+                {
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Up] = null;
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Right] = null;
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Down] = Maze[x, y - 1].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Left] = Maze[x - 1, y].GetComponent<Tile>();
+                }
+                else if (x == 0)
+                {
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Up] = Maze[x, y + 1].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Right] = Maze[x + 1, y].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Down] = Maze[x, y - 1].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Left] = null;
+                }
+                else if (x == Width - 1)
+                {
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Up] = Maze[x, y + 1].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Right] = null;
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Down] = Maze[x, y - 1].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Left] = Maze[x - 1, y].GetComponent<Tile>();
+                }
+                else if (y == 0)
+                {
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Up] = Maze[x, y + 1].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Right] = Maze[x + 1, y].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Down] = null;
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Left] = Maze[x - 1, y].GetComponent<Tile>();
+                }
+                else if (y == Height - 1)
+                {
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Up] = null;
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Right] = Maze[x + 1, y].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Down] = Maze[x, y - 1].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Left] = Maze[x - 1, y].GetComponent<Tile>();
+                }
+                else
+                {
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Up] = Maze[x, y + 1].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Right] = Maze[x + 1, y].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Down] = Maze[x, y - 1].GetComponent<Tile>();
+                    Maze[x, y].GetComponent<Tile>().Neighbor[(int)Facing.Left] = Maze[x - 1, y].GetComponent<Tile>();
+                }
+            }
+
+        }
+    }
+
+
+    void CreateMaze()
+    {
+        List<Tile> TileStack;
+        
+
+    }
+
+    void SelectNeighbor()
+    {
+       
+    }
+
+    void LowerWall()
+    {
+
+    }
+}
---
+++ CowRPG/Assets/Tile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Tile : MonoBehaviour {
+
+    Sprite Vertical, Horizontal, DRelbow, DLelbow, URelbow, ULelbow, Left3, Up3, Down3, Right3, CrossRoad;
+
+    SpriteRenderer CurrentSprite;
+
+    public Transform[] Walls;
+    public Tile[] Neighbor;
+    public bool Visited;
+
+    public int x, y;
+    public GameObject MazeGen;
+	// Update is called once per frame
+	void Start () {
+		
+	}
+    
+}
